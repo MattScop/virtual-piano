450d22f5ff40ce1f6adbea44ff5dfdf01a1b0e5b
MattScop <matteo.scopece@outlook.com>
2022-07-01 12:12:31 +0200

    Restyle and start STEP 3 in JS

--- script.js
+++ script.js
@@ -1,17 +1,31 @@
-// STEP 1: grab keyCode from key press
+// **STEP 1: grab keyCode from key press**
 // window.addEventListener('keyup', (e) => {
 //     console.log(e)
 // })
 
-// STEP 2: play a note
+// **STEP 2: play a note**
+
+// window.addEventListener('keyup', (e) => {
+//     const audio = document.querySelector(`audio[data-key="${e.key}"]`);
+//     if (!audio) return // stop the function if no data-key is found
+//     audio.currentTime = 0;
+//     audio.play();
+// })
+
+// **STEP 3: add class if played**
 
 window.addEventListener('keyup', (e) => {
+    const keyPress = document.querySelector(`div[data-key="${e.key}"]`);
     const audio = document.querySelector(`audio[data-key="${e.key}"]`);
-    if (!audio) return
-    console.log(audio)
-    audio.play();
-    audio.currentTime = 0;
-})
 
+    keyPress.classList.add('keyPressed')
+    keyPress.addEventListener('transitionend', () => {
+        console.log("hello")
+    });
+
+    if (!audio) return // stop the function if no data-key is found
+    audio.currentTime = 0;
+    audio.play();
+})
     
     
