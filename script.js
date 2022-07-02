3b7290ff23e7eea5c118669421ed2d87a22cf5a1
MattScop <matteo.scopece@outlook.com>
2022-07-02 18:39:21 +0200

    Add first version display

--- script.js
+++ script.js
@@ -14,21 +14,32 @@
 
 // **STEP 3: add class if played**
 
+const para = document.createElement('p');
+para.style.cssText = "color: white; padding: 10px; text-align: center; font-size: 20px; letter-spacing: 0.25em; font-style: italic;";
+
+const display = document.querySelector('.display');
+
 window.addEventListener('keydown', (e) => {
     const keyPress = document.querySelector(`div[data-key="${e.key}"]`);
     const audio = document.querySelector(`audio[data-key="${e.key}"]`);
-
-    keyPress.classList.add('keyPressed')
-
+    
+    if (!keyPress) return // stop the function if no data-key is found
+    keyPress.classList.add('keyPressed');
+    
     if (!audio) return // stop the function if no data-key is found
     if (!e.repeat) {
-    audio.currentTime = 0; // rewind the audio from start each press
-    audio.play();
+        audio.currentTime = 0; // rewind the audio from start each press
+        audio.play();
     }
+
+    para.textContent += e.key;
+    display.appendChild(para);
 })
 
 window.addEventListener('keyup', (e) => {
     const keyPress = document.querySelector(`div[data-key="${e.key}"]`);
+
+    if (!keyPress) return
     keyPress.classList.remove('keyPressed')
 })
     
